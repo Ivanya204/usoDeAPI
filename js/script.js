182fde8525a04c2e659a183d1210729fd4b209cd
Ivanya <20240014@ricaldone.edu.sv>
2025-06-05 11:24:02 -0600

    Funcionalidad de agregar casi completa

--- js/script.js
+++ js/script.js
@@ -45,3 +45,49 @@ ObtenerRegistros();
 const modal = document.getElementById("mdAgregar"); //Cuadro de dialogo
 const btnAgregar = document.getElementById("btnAgregar"); //Boton para abrir
 const btnCerrar = document.getElementById("btnCerrarModal"); //Boton cerrar
+
+btnAgregar.addEventListener("click", ()=>{
+    modal.showModal();//Abre el modal cuando a btnAgregar se le hace clic
+});
+
+btnCerrar.addEventListener("click", ()=>{
+    modal.close();
+});
+
+//Agregar un nuevo integrante desde el formulario
+document.getElementById("frmAgregar").addEventListener("submit", async e=> {
+    e.preventDefault();//Evita que los datos se envien por defecto
+    //Capturar los valores del form
+    const nombre = document.getElementById("txtNombre").value.trim();
+    const apellido = document.getElementById("txtApellido").value.trim();
+    const correo = document.getElementById("txtEmail").value.trim();
+
+    //Validacion basica 
+    if (!nombre || !apellido || !correo){
+        alert("Complete todos los campos")
+        return; //Evita que el codigo se siga ejecutando
+    }
+
+    //Llamar a la API para enviat los datos
+    const respuesta = await fetch(API_URL, {
+        method : "POST" , 
+        headers : {'Content-Type' : 'application/json'}, //EL tipo de contenido : JSON
+        body : JSON.stringify({nombre,apellido,correo}) //Quiere decir que un JSON va a ser convertido a una Strinf
+    });
+
+    if(respuesta.ok){
+        //Mensaje de confirmacion
+        alert("El registro fue agregado correctamente")
+        //Limpiar el formulario
+        document.getElementById("frmAgregar").reset();
+        //Cerrar el modal 
+        modal.close();
+
+        //Recargar la tabla
+        ObtenerRegistros();
+    }
+    else{
+        alert("Hubo un error a la hora de guardar el registro")
+    }
+});
+
